2fac79f24a64238616d73ed73b0e4c5610991c3e
toaru <toaru@DESKTOP-DQ9P3NG>
2022-02-28 11:33:40 +0900

    v1.0.0

--- Fire HD Google Play Installer/MainWindow.xaml.cs
+++ Fire HD Google Play Installer/MainWindow.xaml.cs
@@ -25,15 +25,28 @@ namespace Fire_HD_Google_Play_Installer
         private System.Net.WebClient wc = new System.Net.WebClient();
         System.Net.WebClient wc2 = new System.Net.WebClient();//なぜ分けた？？？？
         private ProcessStartInfo si;
-        private bool finish;
         private void Install_Click(object sender, RoutedEventArgs e)
         {
+            n = 0;
             WebClient wc2 = new WebClient();
             if (five.IsChecked == true)
             {
                 int i = 0;
                 Directory.CreateDirectory(@".\Download");
-                string[] Urls = new string[] { "https://d-01.aabstatic.com/1017/google_account_manager_5.1-1743759_androidapksbox.apk", "https://d-01.aabstatic.com/1017/google_services_framework_5.1-1743759_androidapksbox.apk", "https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=2200822", "https://d-01.aabstatic.com/1020/google_play_store_22.4.25-21_androidapksbox.apk" };
+                string[] Urls = new string[] { "https://d-01.aabstatic.com/1017/google_account_manager_5.1-1743759_androidapksbox.apk", "https://d-01.aabstatic.com/1017/google_services_framework_5.1-1743759_androidapksbox.apk", "https://r2-static-assets.androidapksfree.com/sdata/5e3f9bb672ebf370bd6b3faf7e661843/com.google.android.gms_v22.02.21_(000300-428111784)-220221000_Android-4.4.apk", "https://d-01.aabstatic.com/1020/google_play_store_22.4.25-21_androidapksbox.apk" };
+                foreach (string url in Urls)
+                {
+
+                    downloadFileAsync(url, $".\\Download\\{i}.apk");
+                    i++;
+                }
+
+            }
+            else if (seven.IsChecked == true)
+            {
+                int i = 0;
+                Directory.CreateDirectory(@".\Download");
+                string[] Urls = new string[] { "https://appszx.com/wp-content/uploads/2019/08/com.google.android.gsf_.login_7.1.2-25_minAPI23nodpi_appszx.com_.apk", "https://download1586.mediafire.com/1mxova5purcg/2xrhdtnm26df3av/Google+Services+Framework_v9_appszx.com.apk", "https://r2-static-assets.androidapksfree.com/sdata/5e3f9bb672ebf370bd6b3faf7e661843/com.google.android.gms_v22.02.21_(000300-428111784)-220221000_Android-4.4.apk", "https://d-01.aabstatic.com/1020/google_play_store_22.4.25-21_androidapksbox.apk" };
                 foreach (string url in Urls)
                 {
 
@@ -47,85 +60,63 @@ namespace Fire_HD_Google_Play_Installer
 
         private void adb()
         {
-            StreamReader sm = new StreamReader(Path.GetTempPath() + "\\" + "Path.txt");
-            string saveFolder = sm.ReadToEnd();
-            StreamReader sm3 = new StreamReader(Path.GetTempPath() + "\\" + "switch.txt");
-            string mp4_mkv = sm3.ReadToEnd();
-            sm.Close();
-            sm3.Close();
-            this.Dispatcher.Invoke((Action)(() =>
+            int c = 0;
+            MessageBox.Show("タブレットをパソコンに接続してください。\n※固まってしまった場合は強制終了させてください。", "Infomation", MessageBoxButton.OK, MessageBoxImage.Information);
+            si = new ProcessStartInfo(@"adb.exe", $"device");
+            MessageBox.Show("USBデバッグを許可してください。", "Infomation", MessageBoxButton.OK, MessageBoxImage.Information);
+            for (c = 0; c <=3;c++)
             {
-                if (five.IsChecked == true)
+                
+                    
+                    si = new ProcessStartInfo(@"adb.exe", $"install .\\Download\\{c}.apk");
+
+                
+                // ウィンドウ表示を完全に消したい場合
+                // si.CreateNoWindow = true;
+                si.RedirectStandardError = true;
+                si.RedirectStandardOutput = true;
+                si.UseShellExecute = false;
+                using (var proc = new Process())
+                using (var ctoken = new CancellationTokenSource())
                 {
-                    MessageBox.Show("タブレットをパソコンに接続してください。\n※固まってしまった場合は強制終了させてください。","Infomation", MessageBoxButton.OK,MessageBoxImage.Information);
-                    si = new ProcessStartInfo(@"adb.exe", $"install .\\Download\\0.apk");
-                }
-
-
-            }));
-
-            // ウィンドウ表示を完全に消したい場合
-            si.CreateNoWindow = true;
-            si.RedirectStandardError = true;
-            si.RedirectStandardOutput = true;
-            si.UseShellExecute = false;
-            using (var proc = new Process())
-            using (var ctoken = new CancellationTokenSource())
-            {
-
-                proc.EnableRaisingEvents = true;
-                proc.StartInfo = si;
-                // コールバックの設定
-                proc.Exited += (s, ev) =>
-                {
-                    Console.WriteLine($"exited");
-                    this.Dispatcher.Invoke((Action)(() =>
+                    proc.EnableRaisingEvents = true;
+                    proc.StartInfo = si;
+                    // コールバックの設定
+                    proc.Exited += (sender, ev) =>
                     {
-
-                    }));
-                    // プロセスが終了すると呼ばれる
-                    ctoken.Cancel();
-                };
-                // プロセスの開始
-                proc.Start();
-                Task.WaitAll(
-                    Task.Run(() =>
-                    {
-                        while (true)
-                        {
-                            var l = proc.StandardOutput.ReadLine();
-                            this.Dispatcher.Invoke((Action)(() =>
-                            {
-                                adb_Log.Content = l;
-
-                            }));
-                            if (l == null)
-                            {
-                                break;
-                            }
-                            try
-                            {
-                                this.Dispatcher.Invoke((Action)(() =>
-                                {
-
-                                }));
-                            }
-                            catch (Exception)
-                            {
-
-                            }
-                        }
+                        Console.WriteLine($"exited");
+                        
                         n = 0;
+                        // プロセスが終了すると呼ばれる
+                        ctoken.Cancel();
+                    };
+                    // プロセスの開始
+                    proc.Start();
+                    Task.WaitAll(
+                        Task.Run(() =>
+                        {
+                            while (true)
+                            {
+                                var l = proc.StandardOutput.ReadLine();
+                                if (l == null)
+                                {
+                                    break;
+                                }
+                                Console.WriteLine($"stdout = {l}");
 
-                    }),
-                    Task.Run(() =>
-                    {
-                        ctoken.Token.WaitHandle.WaitOne();
-                        proc.WaitForExit();
-                    })
-                );
+                            }
+                        }),
+                        Task.Run(() =>
+                        {
+                            ctoken.Token.WaitHandle.WaitOne();
+                            proc.WaitForExit();
+                        })
+                    );
+                }
             }
 
+
+            MessageBox.Show("処理が終了しました！", "Infomation", MessageBoxButton.OK, MessageBoxImage.Information);
 
 
 
@@ -136,6 +127,7 @@ namespace Fire_HD_Google_Play_Installer
 
         private async void downloadFileAsync(string uri, string outputPath)
         {
+            
             var client = new HttpClient();
             HttpResponseMessage res = await client.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead);
 
@@ -150,11 +142,21 @@ namespace Fire_HD_Google_Play_Installer
             }
             if (n == 3)
             {
-                finish = true;
                 Debug.WriteLine("完了");
                 adb();
 
+
+                DirectoryInfo di = new DirectoryInfo(@".\Download");
+                FileInfo[] files = di.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    file.Delete();
+                }
+                Console.WriteLine("Files deleted successfully");
+                
+
             }
+            
         }
 
 
